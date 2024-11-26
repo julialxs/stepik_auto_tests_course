6108d714caefa9bc23737d5aced6dc0544058ed8
julialxs <aleksahinayu@gmail.com>
2024-11-26 18:49:15 +0400

    git commit -m "1.6_step11 fix"

--- lesson8_step10.py
+++ lesson8_step10.py
@@ -2,22 +2,24 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 
-    link = "https://suninjuly.github.io/registration2.html"
+try:
+    link = "http://suninjuly.github.io/registration2.html"
     browser = webdriver.Chrome()
     browser.get(link)
+
     input1 = browser.find_element(By.CSS_SELECTOR, "div.first_block div.first_class input.first")
     input1.send_keys('aaaaaaa')
     input2 = browser.find_element(By.CSS_SELECTOR, "div.first_block div.second_class input.second")
     input2.send_keys('bbbbbb')
     input3 = browser.find_element(By.CSS_SELECTOR, "div.first_block div.third_class input.third")
-    input2.send_keys('bbbbbb')
+    input3.send_keys('eeee')
     # Отправляем заполненную форму
     button = browser.find_element(By.CSS_SELECTOR, "button.btn")
     button.click()
 
     # Проверяем, что смогли зарегистрироваться
     # ждем загрузки страницы
-    time.sleep(1)
+    time.sleep(5)
 
     # находим элемент, содержащий текст
     welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
@@ -26,8 +28,8 @@ import time
 
     # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
     assert "Congratulations! You have successfully registered!" == welcome_text
-
+finally:
     # ожидание чтобы визуально оценить результаты прохождения скрипта
-    time.sleep(10)
+    time.sleep(5)
     # закрываем браузер после всех манипуляций
     browser.quit()
